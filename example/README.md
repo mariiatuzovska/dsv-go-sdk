933832ac9c330f646ada55ea4ef72c77e6e06aee
Mariia.Tuzovska <Mariia.Tuzovska@c.delinea.com>
2022-09-30 14:25:01 +0300

    add example

---
+++ example/README.md
@@ -0,0 +1,6 @@
+# Password authentication + read secret example
+
+* `$export DSV_BASE_PATH=https://<tenant name>.<server>.com/v1`
+* `$export DSV_USERNAME=<username>`
+* `$export DSV_PASSWORD=<password>`
+* `$export DSV_SECRET_PATH=<secret_path>`
---
+++ example/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/antihax/optional"
+	dsvclient "github.com/mariiatuzovska/dsv-go-sdk/client"
+)
+
+func main() {
+	client := dsvclient.NewAPIClient(&dsvclient.Configuration{
+		BasePath: os.Getenv("DSV_BASE_PATH"),
+	})
+	acsessTokenResponse, httpResp, err := client.TokensApi.Token(context.Background(), "password", &dsvclient.TokensApiTokenOpts{
+		Username: optional.NewString(os.Getenv("DSV_USERNAME")),
+		Password: optional.NewString(os.Getenv("DSV_PASSWORD")),
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d %#v\n", httpResp.StatusCode, acsessTokenResponse)
+
+	client = dsvclient.NewAPIClient(&dsvclient.Configuration{
+		BasePath: os.Getenv("DSV_BASE_PATH"),
+		DefaultHeader: map[string]string{
+			"Authorization": acsessTokenResponse.AccessToken,
+		},
+	})
+	secretResponse, httpResp, err := client.SecretsApi.GetSecret(context.Background(), os.Getenv("DSV_SECRET_PATH"), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%d %#v\n", httpResp.StatusCode, secretResponse)
+}
